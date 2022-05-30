08253108d21e2d8d04aa0980629ef4f7d84c1d72
Thiripura123 <thiripura.sundari@qualitestgroup.com>
2022-05-30 17:24:24 +0530

    This is commit from Person X

--- src/main/java/Qualitest_Thiri/Practice/Parent.java
+++ src/main/java/Qualitest_Thiri/Practice/Parent.java
@@ -4,7 +4,7 @@ public class Parent {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welocme to Java. learning of Class and Object");
+		System.out.println("Welocme to Java. learning of Class and Object- Updated code by X- Git");
 		Method M=new Method();
 		M.headervalidation();
 		System.out.println(M.headervalidation());
